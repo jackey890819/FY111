ef8e18bd1db45e70ced449cb3ee7240681f2bda4
haoping674 <F110151105@nkust.edu.tw>
2022-07-18 10:58:38 +0800

    新增簡易圖表分析

--- FY111/Dtos/LogDto.cs
+++ FY111/Dtos/LogDto.cs
@@ -1,4 +1,7 @@
-﻿namespace FY111.Dtos
+﻿using FY111.Models.FY111;
+using System.Collections.Generic;
+
+namespace FY111.Dtos
 {
     public class AttendeeLogDto
     {
@@ -11,5 +14,12 @@
             this.name = name;
             this.checkin = checkin;
         }
+    }
+
+    public class LogsDto
+    {
+        public List<OperationUnitLog> UnitLog { get; set; }
+        public List<Class> @class { get; set; }
+        public List<LoginLog> LoginLog { get; set; } 
     }
 }
