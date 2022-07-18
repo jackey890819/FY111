f957fd315897c2aeed978e142d4dba5d6e51c721
Yu-0920 <z125088695@gmail.com>
2022-07-18 11:44:27 +0800

    新增檢查點和職災

---
+++ FY111/Controllers/ClassLittleunitsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FY111.Models.FY111;
+
+namespace FY111.Controllers
+{
+    public class ClassLittleunitsController : Controller
+    {
+        private readonly FY111Context _context;
+
+        public ClassLittleunitsController(FY111Context context)
+        {
+            _context = context;
+        }
+
+        // GET: ClassLittleunits
+        public async Task<IActionResult> Index()
+        {
+            var fY111Context = _context.ClassLittleunits.Include(c => c.ClassUnit);
+            return View(await fY111Context.ToListAsync());
+        }
+
+        // GET: ClassLittleunits/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classLittleunit = await _context.ClassLittleunits
+                .Include(c => c.ClassUnit)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (classLittleunit == null)
+            {
+                return NotFound();
+            }
+
+            return View(classLittleunit);
+        }
+
+        // GET: ClassLittleunits/Create
+        public IActionResult Create()
+        {
+            ViewData["ClassUnitId"] = new SelectList(_context.ClassUnits, "Id", "Id");
+            return View();
+        }
+
+        // POST: ClassLittleunits/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClassUnitId,Code,Name,Image")] ClassLittleunit classLittleunit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(classLittleunit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClassUnitId"] = new SelectList(_context.ClassUnits, "Id", "Id", classLittleunit.ClassUnitId);
+            return View(classLittleunit);
+        }
+
+        // GET: ClassLittleunits/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classLittleunit = await _context.ClassLittleunits.FindAsync(id);
+            if (classLittleunit == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClassUnitId"] = new SelectList(_context.ClassUnits, "Id", "Id", classLittleunit.ClassUnitId);
+            return View(classLittleunit);
+        }
+
+        // POST: ClassLittleunits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClassUnitId,Code,Name,Image")] ClassLittleunit classLittleunit)
+        {
+            if (id != classLittleunit.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(classLittleunit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassLittleunitExists(classLittleunit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClassUnitId"] = new SelectList(_context.ClassUnits, "Id", "Id", classLittleunit.ClassUnitId);
+            return View(classLittleunit);
+        }
+
+        // GET: ClassLittleunits/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var classLittleunit = await _context.ClassLittleunits
+                .Include(c => c.ClassUnit)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (classLittleunit == null)
+            {
+                return NotFound();
+            }
+
+            return View(classLittleunit);
+        }
+
+        // POST: ClassLittleunits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var classLittleunit = await _context.ClassLittleunits.FindAsync(id);
+            _context.ClassLittleunits.Remove(classLittleunit);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassLittleunitExists(int id)
+        {
+            return _context.ClassLittleunits.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ FY111/Controllers/OccdisastersManageController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FY111.Models.FY111;
+
+namespace FY111.Controllers
+{
+    public class OccdisastersManageController : Controller
+    {
+        private readonly FY111Context _context;
+
+        public OccdisastersManageController(FY111Context context)
+        {
+            _context = context;
+        }
+
+        // GET: OccdisastersManage
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            if (searchString != null) 
+            {
+                pageNumber = 1;
+            }
+            else 
+            {
+                searchString = currentFilter;
+            }
+            ViewData["CurrentFilter"] = searchString;
+
+            var occdisasters = _context.Occdisasters.Select(x => x);
+            if (!String.IsNullOrEmpty(searchString)) 
+            {
+                occdisasters = occdisasters.Where(s => s.Content.Contains(searchString));
+            }
+            //switch (sortOrder) {
+            //    case "signup_desc":
+            //        occdisasters = occdisasters.OrderByDescending(x => x.SignupEnabled);
+            //        break;
+            //    case "checkin":
+            //        occdisasters = occdisasters.OrderBy(x => x.CheckinEnabled);
+            //        break;
+            //    case "checkin_desc":
+            //        occdisasters = occdisasters.OrderByDescending(x => x.CheckinEnabled);
+            //        break;
+            //    default:
+            //        occdisasters = occdisasters.OrderBy(x => x.SignupEnabled);
+            //        break;
+            //}
+            int pageSize = 3;
+            return View(await PaginatedList<Occdisaster>.CreateAsync(occdisasters.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        // GET: OccdisastersManage/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var occdisaster = await _context.Occdisasters
+                .FirstOrDefaultAsync(m => m.Code == id);
+            if (occdisaster == null)
+            {
+                return NotFound();
+            }
+
+            return View(occdisaster);
+        }
+
+        // GET: OccdisastersManage/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OccdisastersManage/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Code,Content")] Occdisaster occdisaster)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(occdisaster);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(occdisaster);
+        }
+
+        // GET: OccdisastersManage/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var occdisaster = await _context.Occdisasters.FindAsync(id);
+            if (occdisaster == null) {
+                return NotFound();
+            }
+            return View(occdisaster);
+        }
+
+        // POST: OccdisastersManage/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Code,Content")] Occdisaster occdisaster)
+        {
+            if (id != occdisaster.Code) {
+                var originOccdisaster = await _context.Occdisasters.FindAsync(id);
+                _context.Occdisasters.Remove(originOccdisaster);
+                _context.Add(occdisaster);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(occdisaster);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OccdisasterExists(occdisaster.Code))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(occdisaster);
+        }
+
+        // GET: OccdisastersManage/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var occdisaster = await _context.Occdisasters
+                .FirstOrDefaultAsync(m => m.Code == id);
+            if (occdisaster == null)
+            {
+                return NotFound();
+            }
+
+            return View(occdisaster);
+        }
+
+        // POST: OccdisastersManage/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var @occdisaster = await _context.Occdisasters.FindAsync(id);
+            _context.Occdisasters.Remove(@occdisaster);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OccdisasterExists(string id)
+        {
+            return _context.Occdisasters.Any(e => e.Code == id);
+        }
+    }
+}
