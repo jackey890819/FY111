6f72c3d16f40f9c04e7f02e27230be888a64ece7
楊順發 <jackey890819@gmail.com>
2022-07-25 14:40:49 +0800

    Revert "註解部分程式 執行scaffold"
    This reverts commit 204d1ea237683c09102ba0a1d0842f6ac833132a.

--- FY111/Controllers/API/ClassSignupsController.cs
+++ FY111/Controllers/API/ClassSignupsController.cs
@@ -1,135 +1,135 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using FY111.Models.FY111;
-//using Microsoft.AspNetCore.Identity;
-//using FY111.Areas.Identity.Data;
-//using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FY111.Models.FY111;
+using Microsoft.AspNetCore.Identity;
+using FY111.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
 
-//namespace FY111.Controllers.API
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class ClassSignupsController : ControllerBase
-//    {
-//        private readonly FY111Context _context;
-//        private UserManager<FY111User> _userManager;
-//        private SignInManager<FY111User> _signInManager;
+namespace FY111.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassSignupsController : ControllerBase
+    {
+        private readonly FY111Context _context;
+        private UserManager<FY111User> _userManager;
+        private SignInManager<FY111User> _signInManager;
 
-//        public ClassSignupsController(
-//            FY111Context context, 
-//            UserManager<FY111User> userManager,
-//            SignInManager<FY111User> signInManager)
-//        {
-//            _context = context;
-//            _userManager = userManager;
-//            _signInManager = signInManager;
-//        }
+        public ClassSignupsController(
+            FY111Context context, 
+            UserManager<FY111User> userManager,
+            SignInManager<FY111User> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
-//        // GET: api/ClassSignups/5
-//        [HttpGet]
-//        public async Task<ActionResult<IEnumerable<ClassSignup>>> GetClassSignup
-//            ([FromQuery]String memberId="", [FromQuery]int classId=-1)
-//        {
-//            List<ClassSignup> result;
-//            if (memberId !="" && classId != -1)
-//            {
-//                result = await _context.ClassSignups
-//                    .Where(e => e.MemberId == memberId && e.ClassId == classId)
-//                    .ToListAsync();
-//            } else if (memberId != "")
-//            {
-//                result = await _context.ClassSignups
-//                    .Where(e => e.MemberId == memberId)
-//                    .ToListAsync();
-//            }else if (classId != -1)
-//            {
-//                result = await _context.ClassSignups
-//                    .Where(e => e.ClassId == classId)
-//                    .ToListAsync();
-//            }
-//            else
-//            {
-//                result = await _context.ClassSignups
-//                    .Take(10)
-//                    .ToListAsync();
-//            }
+        // GET: api/ClassSignups/5
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClassSignup>>> GetClassSignup
+            ([FromQuery]String memberId="", [FromQuery]int classId=-1)
+        {
+            List<ClassSignup> result;
+            if (memberId !="" && classId != -1)
+            {
+                result = await _context.ClassSignups
+                    .Where(e => e.MemberId == memberId && e.ClassId == classId)
+                    .ToListAsync();
+            } else if (memberId != "")
+            {
+                result = await _context.ClassSignups
+                    .Where(e => e.MemberId == memberId)
+                    .ToListAsync();
+            }else if (classId != -1)
+            {
+                result = await _context.ClassSignups
+                    .Where(e => e.ClassId == classId)
+                    .ToListAsync();
+            }
+            else
+            {
+                result = await _context.ClassSignups
+                    .Take(10)
+                    .ToListAsync();
+            }
 
-//            if (result.Count() == 0)
-//            {
-//                return NotFound();
-//            }
+            if (result.Count() == 0)
+            {
+                return NotFound();
+            }
 
-//            return result;
-//        }
+            return result;
+        }
 
 
-//        // POST: api/ClassSignups
-//        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-//        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-//        [HttpPost]
-//        public async Task<ActionResult<ClassSignup>> PostClassSignup(ClassSignup classSignup)
-//        {
-//            if (!_signInManager.IsSignedIn(User))
-//                return Unauthorized();
-//            // 使用者檢查
-//            if (User.IsInRole("NormalUser"))
-//            {
-//                var user = await _userManager.GetUserAsync(User);
-//                if (user.Id != classSignup.MemberId)
-//                    return BadRequest("普通使用者無法新增他人的報名資料。");
-//            }
-//            // 重複報名檢查
-//            bool check = await _context.ClassSignups
-//                .Where(e => e.Equals(classSignup)).AnyAsync();
-//            if (check)
-//                return BadRequest("已進行報名");
-//            // 新增報名資料
-//            try
-//            {
-//                _context.ClassSignups.Add(classSignup);
-//                await _context.SaveChangesAsync();
-//            }
-//            catch (DbUpdateException)
-//            {
-//                if (ClassSignupExists(classSignup.MemberId))
-//                {
-//                    return Conflict();
-//                }
-//                else
-//                {
-//                    throw;
-//                }
-//            }
-//            return CreatedAtAction("GetClassSignup", new { id = classSignup.MemberId }, classSignup);
-//        }
+        // POST: api/ClassSignups
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<ClassSignup>> PostClassSignup(ClassSignup classSignup)
+        {
+            if (!_signInManager.IsSignedIn(User))
+                return Unauthorized();
+            // 使用者檢查
+            if (User.IsInRole("NormalUser"))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user.Id != classSignup.MemberId)
+                    return BadRequest("普通使用者無法新增他人的報名資料。");
+            }
+            // 重複報名檢查
+            bool check = await _context.ClassSignups
+                .Where(e => e.Equals(classSignup)).AnyAsync();
+            if (check)
+                return BadRequest("已進行報名");
+            // 新增報名資料
+            try
+            {
+                _context.ClassSignups.Add(classSignup);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClassSignupExists(classSignup.MemberId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtAction("GetClassSignup", new { id = classSignup.MemberId }, classSignup);
+        }
 
-//        // DELETE: api/ClassSignups/5
-//        [HttpDelete("{id}")]
-//        public async Task<ActionResult<ClassSignup>> DeleteClassSignup(int id)
-//        {
-//            if (!_signInManager.IsSignedIn(User))
-//                return Unauthorized();
-//            var userId = _userManager.GetUserId(User);
-//            var classSignup = await _context.ClassSignups.Where(e => e.ClassId == id && e.MemberId == userId).SingleOrDefaultAsync();
-//            if (classSignup == null)
-//            {
-//                return NotFound();
-//            }
+        // DELETE: api/ClassSignups/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ClassSignup>> DeleteClassSignup(int id)
+        {
+            if (!_signInManager.IsSignedIn(User))
+                return Unauthorized();
+            var userId = _userManager.GetUserId(User);
+            var classSignup = await _context.ClassSignups.Where(e => e.ClassId == id && e.MemberId == userId).SingleOrDefaultAsync();
+            if (classSignup == null)
+            {
+                return NotFound();
+            }
 
-//            _context.ClassSignups.Remove(classSignup);
-//            await _context.SaveChangesAsync();
+            _context.ClassSignups.Remove(classSignup);
+            await _context.SaveChangesAsync();
 
-//            return classSignup;
-//        }
+            return classSignup;
+        }
 
-//        private bool ClassSignupExists(string id)
-//        {
-//            return _context.ClassSignups.Any(e => e.MemberId == id);
-//        }
-//    }
-//}
+        private bool ClassSignupExists(string id)
+        {
+            return _context.ClassSignups.Any(e => e.MemberId == id);
+        }
+    }
+}
--- FY111/Models/FY111/ClassUnit.cs
+++ FY111/Models/FY111/ClassUnit.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FY111.Resources;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -16,8 +18,11 @@ namespace FY111.Models.FY111
 
         public int Id { get; set; }
         public int ClassId { get; set; }
+        [Display(Name = "Code", ResourceType = typeof(DisplayAttributeResources))]
         public string Code { get; set; }
+        [Display(Name = "ClassUnitName", ResourceType = typeof(DisplayAttributeResources))]
         public string Name { get; set; }
+        [Display(Name = "Image", ResourceType = typeof(DisplayAttributeResources))]
         public string Image { get; set; }
 
         public virtual Class Class { get; set; }
