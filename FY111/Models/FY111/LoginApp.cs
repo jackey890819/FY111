103038226d318876cd4afceb8747805244a28bc1
haoping674 <F110151105@nkust.edu.tw>
2022-07-25 10:42:22 +0800

    資料庫更新

---
+++ FY111/Models/FY111/LoginApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FY111.Models.FY111
+{
+    public partial class LoginApp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Application { get; set; }
+        public string Content { get; set; }
+    }
+}
