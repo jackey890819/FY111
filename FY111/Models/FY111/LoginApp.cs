bbb369ccbe8df9ba35ca18a690dc2f2e18ae47c7
力 莊 <max801024@gmail.com>
2022-08-01 13:17:29 +0800

    修正翻譯

--- FY111/Models/FY111/LoginApp.cs
+++ FY111/Models/FY111/LoginApp.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FY111.Resources;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,8 +10,11 @@ namespace FY111.Models.FY111
     public partial class LoginApp
     {
         public int Id { get; set; }
+        [Display(Name = "Name", ResourceType = typeof(DisplayAttributeResources))]
         public string Name { get; set; }
+        [Display(Name = "Application", ResourceType = typeof(DisplayAttributeResources))]
         public string Application { get; set; }
+        [Display(Name = "Content", ResourceType = typeof(DisplayAttributeResources))]
         public string Content { get; set; }
     }
 }
